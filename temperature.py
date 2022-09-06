7b44843f23c75a6660e96a9cdf012b75b9d948a8
Filip Gliszczyński <filipgliszczynski@gmail.com>
2022-09-06 18:37:21 +0200

    Changes to be committed: 	modified:   temperature.py

--- temperature.py
+++ temperature.py
@@ -1,28 +1,22 @@
 #! /usr/bin/python
 
-import sys
-import argparse
+def CelsiusToFahrenheit(temperature):
+    return temperature * 1.8 + 32
 
-def main(args=None):
-    if args is None:
-        args = sys.argv[1:]
-    temp = args[1]
-    if args[1] == "--help":
-        help()
-    if args[1] == "-h":
-        help()
-    if args[1] == "-f":
-        ctof(temp)
-    if args[1] == "-c":
-        ftoc(temp)
+def FahrenheitToCelsius(temperature):
+    return (temperature - 32) / 1.8
+
+def test(subject):
+    if type(subject) is float:
+        
+def main():
+    menu = input("Choose conversion side Fahrenheit To Celsius[1] Celsius To Fahrenheit [2] ")
+    if menu == 1:
+        temperature = input("Enter Fahrenheit degrees: ")
 
 
-def help():
-    print("Usage: temperature [OPTION] number\n Options: \n -f from celsius to farenheit \n -c from farenheit to celsius")
-def ctof(temp):
-    return (1.8 * temp) + 32
-def ftoc(temp):
-    return (temp + 32)/1.8
+
+
 
 if __name__ == "__name__":
     main()
