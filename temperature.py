60b251ab21717bcc5a4056ff80222ff5329c21d4
Filip Gliszczyński <filipgliszczynski@gmail.com>
2022-09-06 21:40:48 +0200

    Changes to be committed: 	modified:   temperature.py

--- temperature.py
+++ temperature.py
@@ -8,15 +8,33 @@ def FahrenheitToCelsius(temperature):
 
 def test(subject):
     if type(subject) is float:
+        return True
+    elif type(subject) is int:
+        return True
+    else:
+        return False
         
 def main():
     menu = input("Choose conversion side Fahrenheit To Celsius[1] Celsius To Fahrenheit [2] ")
-    if menu == 1:
+    if menu == "1":
         temperature = input("Enter Fahrenheit degrees: ")
+        temperature = int(temperature)
+        if test(temperature) == True:
+            print(str(temperature) + " Fahrenheit is: " + str(FahrenheitToCelsius(temperature)) + " Celsius")
+        else:
+            print("Wrong type")
+            return 2
+    elif menu == "2":
+        temperature = input("Enter Celsius degrees: ")
+        temperature = int(temperature)
+        if test(temperature) == True:
+            print(str(temperature) + " Celsius is: " + str(CelsiusToFahrenheit(temperature)) + " Farenheit")
+        else:
+            print("Wrong type")
+            return 2
+    else:
+        print("Wrong option, try again!")
+        return 1
 
-
-
-
-
-if __name__ == "__name__":
+if __name__ == "__main__":
     main()
